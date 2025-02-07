ade6563bcb487bc87e7421324764d48d21189e06
Lerry Augusto <lerry.pirani@gmail.com>
2025-02-07 15:50:27 -0300

    feat: first commit

---
+++ src/main/java/com/lapdev/eventos_api/domain/address/Address.java
@@ -0,0 +1,31 @@
+package com.lapdev.eventos_api.domain.address;
+
+import com.lapdev.eventos_api.domain.events.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID ID;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
